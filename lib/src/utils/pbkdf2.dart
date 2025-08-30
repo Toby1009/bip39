1940a324f37d0f544f200ea71f41866f0ac4b207
Toby1009 <69885352+Toby1009@users.noreply.github.com>
2025-08-31 05:00:22 +0800

    chore(deps): bump pointycastle from ^3.0.0-nullsafety.2 to ^4.0.0
    ### Description
    - Updated dependency: bumped `pointycastle` from ^3.0.0-nullsafety.2 to ^4.0.0
    
    ### Motivation
    - The old implementation relied on a pre-release (`-nullsafety`) version of pointycastle
    - Upgrading ensures compatibility with the stable ^4.0.0 release
    - This makes the library usable in new projects without dependency conflicts

--- lib/src/utils/pbkdf2.dart
+++ lib/src/utils/pbkdf2.dart
@@ -18,14 +18,12 @@ class PBKDF2 {
     this.blockLength = 128,
     this.iterationCount = 2048,
     this.desiredKeyLength = 64,
-  }) : _derivator =
-            new PBKDF2KeyDerivator(new HMac(new SHA512Digest(), blockLength));
+  }) : _derivator = PBKDF2KeyDerivator(HMac(SHA512Digest(), blockLength));
 
-  Uint8List process(String mnemonic, {passphrase: ""}) {
+  Uint8List process(String mnemonic, {passphrase = ""}) {
     final salt = Uint8List.fromList(utf8.encode(saltPrefix + passphrase));
     _derivator.reset();
-    _derivator
-        .init(new Pbkdf2Parameters(salt, iterationCount, desiredKeyLength));
-    return _derivator.process(new Uint8List.fromList(mnemonic.codeUnits));
+    _derivator.init(Pbkdf2Parameters(salt, iterationCount, desiredKeyLength));
+    return _derivator.process(Uint8List.fromList(mnemonic.codeUnits));
   }
 }
